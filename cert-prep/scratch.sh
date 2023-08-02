9990e54d5f6ee8bf1591318c53a7e8fee0e2c42d
max <maxtwinslow1@gmail.com>
2023-08-02 09:00:12 -0700

    x

--- cert-prep/scratch.sh
+++ cert-prep/scratch.sh
@@ -1,79 +1,22 @@
-
-
-# Set up express
-sudo yum update
-sudo yum upgrade
-sudo yum install -y nodejs
-mkdir my-express-app
-cd my-express-app
-npm init -y
-npm install express --save
-cat > index.js << EOF
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
-
-app.listen(port, () => {
-  console.log(\`Server running on port \${port}\`);
-});
-EOF
-nohup node index.js > app.log 2>&1 &
-
-# Set up Consul
-cat > /etc/consul.d/agent.hcl << EOF
-log_level  = "INFO"
-server     = false
-datacenter = "consul-dc-a"
-primary_datacenter = "consul-dc-a"
-node_name="express-i-08567e8524d90b2c3"
-leave_on_terminate = true
-
-ui_config {
-  enabled = true
-}
-
-# Gossip Encryption - generate key using consul keygen
-encrypt            = "pCOEKgL2SYHmDoFJqnolFUTJi7Vy+Qwyry04WIZUupc="
-data_dir           = "/opt/consul/data"
-
-# Agent Network Configuration
-client_addr    = "0.0.0.0"
-bind_addr      = "0.0.0.0"
-advertise_addr = "{{ GetPublicIP }}"
-
-retry_join = ["provider=aws tag_key=consul tag_value=join region=us-west-2"]
-
-connect {
-  enabled = true
-}
-
-acl {
-  enabled = true
-  default_policy = "allow"
-  down_policy = "extend-cache"
-}
-EOF
-
+# Register an Express Service and Health Check via API
 cat > service-register-payload.json << EOF
 {
-    "ID": "i-08567e8524d90b2c3",
-    "name": "express-server-i-08567e8524d90b2c3",
+    "id": "i-093367e3e17c3d9a1",
+    "name": "express-server-i-093367e3e17c3d9a1",
     "tags": ["api-register", "express"],
-    "port": 3000
+    "port": 3000,
+    "check": {
+      "name": "Express Server Available on Port 3000",
+      "interval": "10s",
+      "timeout": "2s",
+      "tcp": "localhost:3000"
+    }
 }
 EOF
 
 curl \
     --request PUT \
     --data @service-register-payload.json \
-    10.0.1.219:8500/v1/agent/service/register
-
-# Register an Express Service via API
-
-# Register via CLI
-
-# Register via Configuration
+    10.0.2.109:8500/v1/agent/service/register
+  
+  
---
+++ cert-prep/startup-express.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+export INSTANCE_ID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .instanceId)
+
+adduser consul
+usermod -a -G systemd-journal consul
+echo 'consul ALL=(ALL:ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
+
+yum update
+yum upgrade
+yum install -y nodejs
+mkdir /home/consul/my-express-app
+cd /home/consul/my-express-app
+npm init -y
+npm install express --save
+cat > /home/consul/my-express-app/index.js << EOF
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(\`Server running on port \$${port}\`);
+});
+EOF
+nohup node /home/consul/my-express-app/index.js > /home/consul/app.log 2>&1 &
+
+curl --silent -Lo /tmp/consul.zip https://releases.hashicorp.com/consul/${consul_version}/consul_${consul_version}_linux_amd64.zip
+unzip /tmp/consul.zip
+mv consul /usr/bin
+rm -f /tmp/consul.zip
+
+mkdir -p /opt/consul/data/
+chown -R consul:consul /opt/consul/data
+
+mkdir /etc/consul.d/
+echo -e "CONSUL_LICENSE=${consul_license}" > /etc/consul.d/env
+chown -R consul:consul /etc/consul.d/
+
+# Copy .ssh keys from ec2-user to consul user so you can ssh
+# into the ec2 instance as consul.
+mkdir -p /home/consul/.ssh
+cat /home/ec2-user/.ssh/authorized_keys > /home/consul/.ssh/authorized_keys
+chown -R consul:consul /home/consul/.ssh
+chmod 700 /home/consul/.ssh
+chmod 600 /home/consul/.ssh/authorized_keys
+
+cat > /etc/systemd/system/consul.service << EOF
+[Unit]
+Description="HashiCorp Consul - A service mesh solution"
+Documentation=https://www.consul.io/
+Requires=network-online.target
+After=network-online.target
+ConditionFileNotEmpty=/etc/consul.d/agent.hcl
+
+[Service]
+Type=notify
+User=consul
+Group=consul
+EnvironmentFile=/etc/consul.d/env
+ExecStart=/usr/bin/consul agent -config-dir=/etc/consul.d/
+ExecReload=/usr/bin/consul reload
+KillMode=process
+Restart=on-failure
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /etc/consul.d/agent.hcl << EOF
+log_level  = "INFO"
+server     = false
+datacenter = "consul-dc-a"
+primary_datacenter = "consul-dc-a"
+node_name="express-$INSTANCE_ID"
+encrypt            = "pCOEKgL2SYHmDoFJqnolFUTJi7Vy+Qwyry04WIZUupc="
+data_dir           = "/opt/consul/data"
+client_addr    = "0.0.0.0"
+retry_join = ["provider=aws tag_key=consul tag_value=join region=us-west-2"]
+leave_on_terminate = false
+connect {
+  enabled = true
+}
+ui_config {
+  enabled = true
+}
+acl {
+  enabled = true
+  default_policy = "allow"
+  down_policy = "extend-cache"
+}
+EOF
+
+cat > /etc/consul.d/service.hcl << EOF
+node_name = "$INSTANCE_ID"
+service {
+    name = "web-server-$INSTANCE_ID"
+    tags = ["express", "$INSTANCE_ID"]
+    port = 3000
+    check = {
+        name = "Express Server Available on Port 3000"
+        tcp = "localhost:3000"
+        interval = "10s"
+        timeout = "2s"
+    }
+}
+EOF
+
+# Create bash helper commands
+cat > /etc/profile.d/consul.sh << EOF
+export PS1="\[\033[0;31m\]\u@\[\033[0m\]$INSTANCE_ID "
+alias nukeconsul="sudo rm -rf /opt/consul/*"
+alias cl="journalctl -fu consul"
+alias pc="cat /etc/consul.d/config.hcl"
+alias vc="sudo vim /etc/consul.d/*"
+EOF
+
+systemctl start consul
